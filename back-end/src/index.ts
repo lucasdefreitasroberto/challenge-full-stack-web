ff88bd8796d3a3f3a01d7812a877f7538cf2abeb
MarcusDias-r <marcusdias22@hotmail.com>
2024-01-15 16:32:59 -0300

    chore: starting db connection before server

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,8 +1,11 @@
+import { prisma } from "./config/prisma-client";
 import app from "./http/app";
 
 const port = Number(process.env.PORT || 3000);
 const host = "0.0.0.0";
 
-app().listen(port, host, () => {
-	console.info(`Server running on http://${host}:${port}`);
+prisma.$connect().then(() => {
+	app().listen(port, host, () => {
+		console.info(`Server running on http://${host}:${port}`);
+	});
 });
