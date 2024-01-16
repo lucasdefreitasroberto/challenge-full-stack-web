d45f0b0437c9b20b54834a402602fefdba29cba0
MarcusDias-r <marcusdias22@hotmail.com>
2024-01-15 22:23:47 -0300

    chore: setting routes

--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -1,21 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
 import HomeView from '../views/HomeView.vue'
+import StudentsView from '../views/StudentsView.vue'
+import DashboardLayout from '../components/DashboardLayout.vue'
+
+const loginBeforeEnter = () => {
+    const accessToken = localStorage.getItem('accessToken')
+    return accessToken ? { name: 'dashboard' } : true
+}
+
+const dashboardBeforeEnter = () => {
+    const accessToken = localStorage.getItem('accessToken')
+    return accessToken ? true : { name: 'login' }
+}
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
+
     routes: [
         {
-            path: '/',
-            name: 'home',
-            component: HomeView
+            name: 'login',
+            path: '/login',
+            component: LoginView,
+            // beforeEnter: loginBeforeEnter
         },
         {
-            path: '/about',
-            name: 'about',
-            // route level code-splitting
-            // this generates a separate chunk (About.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import('../views/AboutView.vue')
+            path: '/',
+            name: 'dashboard',
+            redirect: 'home',
+            component: DashboardLayout,
+            // beforeEnter: dashboardBeforeEnter,
+            children: [
+                {
+                    path: '/home',
+                    name: 'home',
+                    component: HomeView
+                },
+                {
+                    path: '/students',
+                    name: 'students',
+                    component: StudentsView
+                }
+            ]
         }
     ]
 })
