9d52b6e7e446d0167f6c94c24ff55b3257b426bf
MarcusDias-r <marcusdias22@hotmail.com>
2024-01-18 13:11:23 -0300

    chore: removing swagger

--- back-end/src/http/routes/index.ts
+++ back-end/src/http/routes/index.ts
@@ -1,18 +1,11 @@
 import cors from "cors";
 import { Router } from "express";
 import apiRoutes from "./api.routes";
-import swaggerUi from "swagger-ui-express";
-import swaggerDocs from "../../../swagger.json";
 
 export default (): Router => {
 	const router: Router = Router();
-	const secret: string = process.env.JWT_SECRET || "secret";
 
 	router.use(cors());
-
-	// swagger
-	router.use("/api-docs", swaggerUi.serve);
-	router.get("/api-docs", swaggerUi.setup(swaggerDocs));
 
 	// api-routes
 	router.use("/api", apiRoutes());
